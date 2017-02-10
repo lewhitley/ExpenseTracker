f7af2df7ecfb0862c4a5462c93a0c9d981a02533
Lindsey Whitley <lewhitley@gmail.com>
2017-02-10 01:15:45 -0800

    add expense reducer and middleware to root

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
+import ExpenseReducer from './expense_reducer';
 
 const RootReducer = combineReducers({
-  session: SessionReducer
+  session: SessionReducer,
+  expenses: ExpenseReducer
 });
 
 export default RootReducer;
