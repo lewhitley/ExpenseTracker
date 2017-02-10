069f320213e64506a6ef3e24501e874f22d4388b
Lindsey Whitley <lewhitley@gmail.com>
2017-02-09 23:22:53 -0800

    fix expense routes - was singular

--- app/controllers/api/expenses_controller.rb
+++ app/controllers/api/expenses_controller.rb
@@ -10,6 +10,48 @@ class Api::ExpensesController < ApplicationController
     end
   end
 
+  def update
+    @expense = Expense.find(params[:id])
+    if current_user.id == @expense.user_id
+      if @expense.update(expense_params)
+        @expenses = Expense.find_by_user_id(current_user.id)
+        render :index
+      else
+        render json: @expense.errors.full_messages, status: 422
+      end
+    else
+      render json: ["No user currently logged in or not the owner of the expense"], status: 404
+    end
+  end
+
+  def index
+    #need more
+    if (current_user.id == @expenses.user_id) || (current_user.admin)
+      render :index
+    else
+      render json: ["No user currently logged in or not the owner of the expense"], status: 404
+    end
+  end
+
+  def show
+    @expense = Expense.find(params[:id])
+    if (current_user.id == @expense.user_id) || (current_user.admin)
+      render :show
+    else
+      render json: ["No user currently logged in or not the owner of the expense"], status: 404
+    end
+  end
+
+  def destroy
+    @expense = Expense.find(params[:id])
+    if current_user.id == @expense.user_id
+      @expense.destroy
+      render :index
+    else
+      render json: ["No user currently logged in or not the owner of the expense"], status: 404
+    end
+  end
+
   def expense_params
     params.require(:expense).permit(:user_id, :amount, :description)
   end
